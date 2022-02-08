354fb4930b8d598d11f95172abf4c076389732b8
Muqris Danish Bin Mokhtar <73147403+MuqrisDanish@users.noreply.github.com>
2022-02-08 18:11:24 +0800

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return(
     <Container>
       <Section
-          title="Model S"
+          title="Model U"
           descriptiopn="Order Online for Touchless Delivery"
           backgroundImg="model-s.jpg"
           leftBtnText="Custom order"
